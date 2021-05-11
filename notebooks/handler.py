84a9f801c0ac155b579ac4d67c34f47652ddbeb7
eapuertoc <eapc@ier.unam.mx>
2021-05-11 03:27:39 -0500

    Nuevas modificaciones para generalizar el uso del código para cualquier objeto del archivo OSM  - Se corrigieron errores en libreta  - Se añadió información de funciones "open_osm" y "objects_to_list" para mayor comprensión del funcionamiento del código.  - Se agregó la salida de los typos de objetos que contiene el archivo a la función "objects_to_list".  - Se unen las dos funciones de cargar archivos en una única "load_dict" y se agrega atributo para definir formato.  - Se cierra flujo de apertura de archivo en "open_osm".

--- notebooks/handler.py
+++ notebooks/handler.py
@@ -7,25 +7,51 @@ import pickle
 import os
 
 
-def __open_osm(osm_file): 
+def __open_osm(osm_file):
+    """Read an OSM file and save contents in a list
+    
+    Parameters:
+    -----------
+    osm_file -- File with OSM format
+    
+    Returns:
+    --------
+    osm -- list with lines of the read file
+    """
     archivo = open(osm_file,"r")
     osm = archivo.readlines()
-#     print(osm)
+    archivo.close()
     return osm
+
 def __objects_to_list(osm):
+    """Converts a list with OSM contents in a list with lists of every object and another list with the types of these objects
+    
+    Parameters:
+    -----------
+    osm --  osm list
+    
+    Returns:
+    --------
+    objects_in_lists -- list with lists of every object in the osm list
+    objType_list -- list with the types of objects within the osm list
+    """
     objeto = []
+    objType_list = [] #Nuevo elemento incluido para guardar tipos de objetos contenidos
     coma   = []
     for i,renglon in enumerate(osm):
-        if renglon.startswith("OS:"): #localiza el inicio de un objeto
+        if renglon.startswith("OS:"): #localiza el índice del inicio de un objeto
+            typeobj = renglon.split(":")[1].split(",")[0]
+            if typeobj not in objType_list: #Si el tipo no está incluido se añade a la lista
+                objType_list.append(typeobj)
             objeto.append(i)
-        if ";" in renglon:  #localiza el final de un objeto
+        if ";" in renglon:  #localiza índice del final de un objeto
             coma.append(i)
-    osm_lista = []
-    for i,j in zip(objeto,coma):
-        
-        osm_lista.append(osm[i:j+1]) # el +1 porque las listas no son inclusivas
-    return osm_lista    
+    objects_in_lists = []
+    for i,j in zip(objeto,coma): 
+        objects_in_lists.append(osm[i:j+1]) # el +1 porque las listas no son inclusivas
+    return objects_in_lists, objType_list 
 
+        
 def __separate_objects_rest(osm_list):
 
     OSM_OBJETO = "OS:Material,\n"
@@ -102,15 +128,14 @@ def __create_dictionary_materials(object_list_cleaned):
     return diccionario
 
 
-def save_dict(file,dictionary,handle=False):
-    '''Write object to a JSON  file.
+def save_dict(file,dictionary, handle=False):
+    """Write object to a JSON  file.
     Parameters
     ----------
-    file : str or file handle to save json file with the dictionary.
-    dictionary  : Python dictionary obtained with get_dictionary_material
-    handle = False, writes dictionary without handle
-
-    '''
+    file -- str or file handle to save json file with the dictionary.
+    dictionary -- Python dictionary obtained with get_dictionary_material
+    handle -- False, writes dictionary without handle
+    """
     if handle:
         archivo = open(file, "w")
         json.dump(dictionary,archivo,indent=2)
@@ -128,32 +153,29 @@ def save_dict(file,dictionary,handle=False):
         archivo.close()
 
 
-def load_dict_json(file):
-    '''Returns a dictionary loaded from JSON file
-    Parameters
+def load_dict(file, format="json"):
+    """Returns a dictionary loaded from JSON file
+    Parameters:
     ----------
-    file : File handle to OPEN json file
-    with the dictionary.
+    file -- File handle to OPEN json file with the dictionary.
+    format -- Indicates the format of the file loaded
     
-    Returns
+    Returns:
     -------
-    dictionary 
-        dictionary containing the JSON file of the OSM file
-
-    '''
-    with open(file) as json_file:
-        dictionary = json.load(json_file)
+    dictionary -- dictionary containing the data of the specified file
+    """
+    dictionary = {}
+    if (format == "json"):
+        with open(file) as json_file:
+            dictionary = json.load(json_file)
+    elif (format == "osm"):
+        osm      = __open_osm(file)
+        osm_list = __objects_to_list(osm)[0]
+        object_list, rest_list  = __separate_objects_rest(osm_list)
+        object_list = __clean_objects(object_list)
+  #     rest_list   = __clean_objects(rest_list)
+        dictionary = __create_dictionary_materials(object_list)
     return dictionary
-
-
-def load_dict_osm(osm_file):
-    osm      = __open_osm(osm_file)
-    osm_list = __objects_to_list(osm)
-    object_list, rest_list  = __separate_objects_rest(osm_list)
-    object_list = __clean_objects(object_list)
-#     rest_list   = __clean_objects(rest_list)
-    diccionario = __create_dictionary_materials(object_list)
-    return diccionario
 
 def update_dict(old_dict, new_dict):
     for material in new_dict.keys():
@@ -227,7 +249,7 @@ def __merge_osm(new_file,file1="tmp.osm",file2="tmp2.osm"):
     
 def update_osm(osm_file,new_dict,delete=True,new_file="actualizado.osm"):
     osm      = __open_osm(osm_file)
-    osm_list = __objects_to_list(osm)
+    osm_list = __objects_to_list(osm)[0]
     object_list, rest_list  = __separate_objects_rest(osm_list)
     object_list = __clean_objects(object_list)
 #     rest_list   = __clean_objects(rest_list)
